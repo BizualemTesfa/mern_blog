2652f5db24fb46034270e3cff163af91e73c4d18
BizualemTesfa <bizualemtesfa1@gmail.com>
2024-10-29 17:10:26 +0300

    modify modules

--- api/index.js
+++ api/index.js
@@ -12,3 +12,7 @@ const app = express();
 app.listen(3000, () => {
     console.log('surver is running on port 3000!');
 });
+
+app.get('/test', (req, res)=>{
+    res.json({message:'API is working'});
+});
